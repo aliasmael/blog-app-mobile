4621c6f98a65f1e8a1130cb2309505f5486139eb
aliasmael <alilion22@ymail.com>
2018-01-19 18:52:20 +0200

    Styling sidebar header and menu items

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,75 @@
 import * as React from 'react'
 import { Component } from 'react'
-import { Text } from 'react-native'
-import { Content } from 'native-base'
+import { Container, Content, Text, List, ListItem, Header, Icon, Body } from 'native-base'
 import style from './style'
+import { User } from '../../models/Models'
+import { MenuItem } from './Models'
+import { Image, View } from 'react-native';
 
 interface ISidebarProps {
-	
+	navigation: any
+}
+
+const menuItems: MenuItem[] = [
+	{
+		title: 'Profile',
+		icon: 'person',
+		navigation: 'Profile'
+	},
+	{
+		title: 'Dashboard',
+		icon: 'home',
+		navigation: 'Home'
+	},
+	{
+		title: 'Explore',
+		icon: 'search',
+		navigation: 'Explore'
+	}
+]
+
+const user: User = {
+	id: 'user-id',
+	username: 'aliismael',
+	firstname: 'Ali',
+	lastname: 'Ismael',
+	title: 'Software Developer',
+	company: '',
+	profileimage: require('../../../assets/images/user_default.png'),
 }
 
 export default class Sidebar extends Component<ISidebarProps> {
 	render() {
 		return (
-			<Content style={style.sidebar}>
-				<Text>Drawer</Text>
-			</Content>
+			<Container style={style.sidebar}>
+				<Content>
+					<Header style={style.sidebarHeader}>
+						<Image 
+							source={require('../../../assets/images/user_default.png')} 
+							style={style.profileImage}
+							/>
+						<Body style={style.sidebarBody}>
+							<Text style={style.userInfo}>{user.firstname + ' ' + user.lastname}</Text>
+							<Text style={style.userInfo} note>{user.title}</Text>
+							<Text style={style.username} note>@{user.username}</Text>
+						</Body>
+					</Header>
+					<List
+						dataArray={menuItems}
+						renderRow={(item: MenuItem) => {
+							return (
+								<ListItem
+									button
+									style={style.menuItem}
+									onPress={() => this.props.navigation.navigate(item.navigation)}>
+									<Icon style={style.menuItemIcon} name={item.icon} />
+									<Text style={style.menuItemText}>{item.title}</Text>
+								</ListItem>
+							);
+						}}
+					/>
+				</Content>
+			</Container>
 		);
 	}
 }
--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,9 +1,54 @@
 import { StyleSheet } from 'react-native'
 
+const backgroundColor = '#ffffff'
+const textColor = '#0e0a1a'
+const activeColor = '#944a8e'
+
 const style = StyleSheet.create({
 	sidebar: {
-		backgroundColor: '#fff'
-	}
+		backgroundColor: backgroundColor
+	},
+	sidebarHeader: {
+		backgroundColor: backgroundColor,
+		height: 110,
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderBottomColor: activeColor		
+	},
+	sidebarBody: {
+		flex: 1, 
+		justifyContent: 'center', 
+		alignItems: 'center'
+	},
+	profileImage: {
+		width: 40,
+		height: 40,
+		marginRight: 10
+	},
+	userInfo: {
+		color: textColor,
+		textAlign: 'center'
+	},
+	username: {
+		color: activeColor,
+		textAlign: 'center'
+	},
+	menuItem: {
+		backgroundColor: backgroundColor,
+		borderBottomColor: activeColor
+	},
+	menuItemText: {
+		color: textColor
+	},
+	menuItemIcon: {
+		color: textColor,
+		marginRight: 10
+	},
+	subItem: {
+		backgroundColor: '#333a49'
+	},
 })
 
 export default style
